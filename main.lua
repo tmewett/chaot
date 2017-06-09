d4ebf108413c85abd13734e521d3dded41f927d3
Tom M <tom@collider.in>
2017-06-09 21:35:09 +0100

    Calculate player heading angle from mouse

--- main.lua
+++ main.lua
@@ -4,13 +4,15 @@ function love.load()
 	arena = require 'arena'
 	midx, midy = 400, 300
 
-	pl = {x=640, y=480, a=0}
+	pl = {x=640, y=480, heading=0}
 	map = arena.new()
 	map[2][4] = 2
 end
 
 function love.update(dt)
-
+	mdx = math.min(mouse.getX()-320, .01)
+	mdy = mouse.getY()-240
+	pl.heading = math.asin(mdy/mdx)
 end
 
 function love.draw()
