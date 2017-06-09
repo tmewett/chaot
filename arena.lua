8cc835a4a63d1a96fd7b58bf75eda8bfd2a4e4ac
Dan Shields <d.shieldsuk@gmail.com>
2017-06-09 23:49:50 +0100

    Switched map x and y values

--- arena.lua
+++ arena.lua
@@ -7,20 +7,20 @@ local color = {
 
 function M.new()
 	local a = {}
-	for y = 1, 8 do
-		a[y] = {}
-		for x = 1, 8 do
-			a[y][x] = 1
+	for x = 1, 8 do
+		a[x] = {}
+		for y = 1, 8 do
+			a[x][y] = 1
 		end
 	end
 	return a
 end
 
 function M:draw(x0, y0)
-	for y = 1, 8 do
-		for x = 1, 8 do
-			gfx.setColor(color[self[y][x]])
-			-- -1 and -2 to make grid lines
+	for x = 1, 8 do
+		for y = 1, 8 do
+			gfx.setColor(color[self[x][y]])
+			-- +1 and -2 to make grid lines
 			gfx.rectangle('fill', x0+(x-1)*160 +1, y0+(y-1)*160 +1, 160-2, 160-2)
 		end
 	end
