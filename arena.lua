d177f25c93b1401812059429ec50ca038e5c7153
Tom M <tom@collider.in>
2017-06-09 20:31:16 +0100

    Make prototype arena much larger and colored

--- arena.lua
+++ arena.lua
@@ -1,21 +1,27 @@
 local M = {}
 
+local color = {
+	[1]={50,50,50},
+	[2]={255,50,50}
+}
+
 function M.new()
 	local a = {}
 	for y = 1, 8 do
 		a[y] = {}
 		for x = 1, 8 do
-			a[y][x] = 0
+			a[y][x] = 1
 		end
 	end
 	return a
 end
 
-function M.draw(A, x0, y0)
+function M:draw(x0, y0)
 	for y = 1, 8 do
 		for x = 1, 8 do
+			gfx.setColor(color[self[y][x]])
 			-- -1 and -2 to make grid lines
-			gfx.rectangle('fill', x0+x*50 +1, y0+y*50 +1, 50-2, 50-2)
+			gfx.rectangle('fill', x0+(x-1)*160 +1, y0+(y-1)*160 +1, 160-2, 160-2)
 		end
 	end
 end
--- main.lua
+++ main.lua
@@ -11,5 +11,6 @@ function love.update(dt)
 end
 
 function love.draw()
+	map[2][4] = 2
 	arena.draw(map, 0, 0)
 end
