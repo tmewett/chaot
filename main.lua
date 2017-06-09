4c089e7d857f3f71aebb5372ee831ab27f64b200
Tom M <tom@collider.in>
2017-06-09 21:13:11 +0100

    Add a stationary "player" with centred camera

--- main.lua
+++ main.lua
@@ -1,9 +1,12 @@
-
 function love.load()
 	gfx = love.graphics
+	mouse = love.mouse
 	arena = require 'arena'
+	midx, midy = 400, 300
 
+	pl = {x=640, y=480, a=0}
 	map = arena.new()
+	map[2][4] = 2
 end
 
 function love.update(dt)
@@ -11,6 +14,11 @@ function love.update(dt)
 end
 
 function love.draw()
-	map[2][4] = 2
+	gfx.push()
+	gfx.translate(midx-pl.x, midy-pl.y)
 	arena.draw(map, 0, 0)
+	gfx.pop()
+
+	gfx.setColor(20, 200, 255)
+	gfx.circle('fill', midx, midy, 40)
 end
